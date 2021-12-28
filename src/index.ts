981d48965dbfded2a54c09c23d8ce2ab6afb1112
Keithcaiqian <653082189@qq.com>
2021-12-28 22:11:56 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-function add(a:number,b:number){
-    return {
-        res: a + b
+export class Tank {
+    protected name: string = "坦克"
+    sayHello() {
+        console.log(`我是一个${this.name}`)
     }
 }
+export class PlayerTank extends Tank {
+    name: string = "玩家坦克"
+    life: number = 5;
 
-let res = add(5,6);
+    sayHello() {
+        console.log("啦啦啦啦");
+    }
+
+    test() {
+        super.sayHello();
+        this.sayHello();
+    }
+}
+export class EnemyTank extends Tank {
+    name: string = "敌方坦克"
+    health: number = 1;
+}
+export class BossTank extends EnemyTank{
+   
+}
+
+const t = new BossTank();
