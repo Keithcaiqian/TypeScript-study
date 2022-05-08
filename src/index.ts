8dbd790c03972a15ccf47400ba92c86a50865285
Keithcaiqian <653082189@qq.com>
2022-05-08 08:56:55 +0800

    装饰器完成

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,28 @@
-//类装饰器
+import { classDescriptor, propDescriptor, printObj } from "./Descriptor";
 
-function test(target: new (...args:any[]) => object){
-    console.log('test')
-}
+@classDescriptor("文章")
+class Article {
+    
+    @propDescriptor("标题")
+    title:string
 
-function test2(){
-    console.log('test2')
-    return function(target: new (...args:any[]) => object){
-        console.log('test3')
+    @propDescriptor("内容")
+    content:string
+
+    @propDescriptor("日期")
+    date:Date
+
+    constructor(){
+        this.title = "xxxx";
+        this.content = "asdfasdfasdfasdfasdf";
+        this.date = new Date();
     }
 }
 
-@test
-@test2()
-class B {
+const ar = new Article();
+// ar.title = "xxxx";
+// ar.content = "asdfasdfasdfasdfasdf";
+// ar.date = new Date();
 
-}
-
-// 成员装饰器和属性装饰器
-function enumerable(target: any, key: string, descriptor: PropertyDescriptor) {
-    // console.log(target, key, descriptor);
-    descriptor.enumerable = true;
-}
-
-function useless(target: any, key: string, descriptor: PropertyDescriptor) {
-    descriptor.value = function () {
-        console.warn(key + "方法已过期");
-    }
-}
-
-function member(target:any, key: string){
-    console.log(target,key)
-}
-
-class A {
-    @member
-    a:string;
-
-    @member
-    static b:string;
-
-    @enumerable
-    @useless
-    method1() {
-        console.log("method1");
-    }
-
-    @enumerable
-    method2() {
-
-    }
-}
-
-const a = new A();
-a.method1();
+printObj(ar);
 
