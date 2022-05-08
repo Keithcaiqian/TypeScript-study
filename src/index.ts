51715a84d4e9d686541bac439d5533504bcd3e07
Keithcaiqian <653082189@qq.com>
2022-05-08 09:36:53 +0800

    三个关键字

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,55 @@
-import { classDescriptor, propDescriptor, printObj } from "./Descriptor";
+//---------------------- typeof ---------------------
+const a: string = 'asss'
+const b: typeof a = 's'
 
-@classDescriptor("文章")
-class Article {
-    
-    @propDescriptor("标题")
-    title:string
-
-    @propDescriptor("内容")
-    content:string
-
-    @propDescriptor("日期")
-    date:Date
-
-    constructor(){
-        this.title = "xxxx";
-        this.content = "asdfasdfasdfasdfasdf";
-        this.date = new Date();
-    }
+class User {
+    loginId: string
+    loginPwd: string
 }
 
-const ar = new Article();
-// ar.title = "xxxx";
-// ar.content = "asdfasdfasdfasdfasdf";
-// ar.date = new Date();
+// typeof User 也就是User构造函数 如果直接写User代表User的实例
+function createUser(cls: typeof User): User{
+    return new cls()
+}
 
-printObj(ar);
+const u = createUser(User);
+
+//---------------------- keyof ---------------------
+
+interface Information {
+    loginId: string,
+    loginPwd: string,
+    age: number
+}
+
+function printUserInformation(obj: Information, prop: keyof Information){
+    console.log(obj[prop])
+}
+const user: Information = {
+    loginId: '23',
+    loginPwd:'34',
+    age: 14
+}
+printUserInformation(user,'age');
+
+//---------------------- keyof ---------------------
+type Obj = {
+    readonly [p in keyof Information]: Information[p]
+}
+
+type Obj2<T> = {
+    readonly [p in keyof T]: T[p]
+}
+
+const u1: Obj = {
+    loginId: '23',
+    loginPwd:'34',
+    age: 14
+} 
+const u2: Obj2<Information> = {
+    loginId: '23',
+    loginPwd:'34',
+    age: 14
+} 
+
 
